fea1ebbfd66ba6f022e0fa51ebd7d9aa9a8d9aa6
Noah Hood <hood.noah@icloud.com>
2025-02-06 12:31:02 -0500

    switch to kubernetes client for secrets management rather than Go environment variables

--- internal/PorkBunSolver/porkbunSolver.go
+++ internal/PorkBunSolver/porkbunSolver.go
@@ -3,11 +3,16 @@ package porkbunsolver
 import (
 	// external
 
+	"context"
+	"fmt"
 	"strings"
 
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook"
 	cmacme "github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/go-logr/logr"
+
+	// k8s
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
@@ -24,7 +29,39 @@ type PorkBunSolver struct {
 var _ webhook.Solver = &PorkBunSolver{}
 
 // dummy inialization function; no need to get secrets from kubernetes
-func (s *PorkBunSolver) Initialize(_ *rest.Config, _ <-chan struct{}) error {
+func (s *PorkBunSolver) Initialize(kubeConfig *rest.Config, stopCh <-chan struct{}) error {
+	// build kubeClient
+	kubeClient, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		return fmt.Errorf("failed to build kube client: %w", err)
+	}
+	s.KubeClient = kubeClient
+
+	// retrieve porkbun credentials from a known Secret; namespace will be hardcoded as will secret
+	secretName := "porkbun-credentials"
+	secretNameSpace := "cert-manager"
+
+	secret, err := kubeClient.CoreV1().Secrets(secretNameSpace).Get(context.TODO(), secretName, metav1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to retrieve porkbun credentials: %w", err)
+	}
+
+	apiKey := string(secret.Data["PORKBUN_API_KEY"])
+	secretKey := string(secret.Data["PORKBUN_SECRET_KEY"])
+
+	if apiKey == "" {
+		return fmt.Errorf("missing required field PORKBUN_API_KEY in secret %s/%s", secretNameSpace, secretName)
+	}
+
+	if secretKey == "" {
+
+		return fmt.Errorf("missing required field PORKBUN_SECRET_KEY in secret %s/%s", secretNameSpace, secretName)
+	}
+
+	// create porkbun client
+	s.PorkBunClient = porkbunclient.NewPorkBunClient(s.logger, apiKey, secretKey)
+	s.logger.Info("PorkBunSolver initialized successfully with PorkBun credentials from Kubernetes secrets.")
+
 	return nil
 }
 
@@ -33,11 +70,10 @@ func (s *PorkBunSolver) Name() string {
 }
 
 // ctor for PorkBunSolver
-func NewPorkBunSolver(logger logr.Logger, kubeClient kubernetes.Interface, porkBunClient *porkbunclient.PorkBunClient) *PorkBunSolver {
+func NewPorkBunSolver(logger logr.Logger, kubeClient kubernetes.Interface) *PorkBunSolver {
 	return &PorkBunSolver{
-		logger:        logger,
-		KubeClient:    kubeClient,
-		PorkBunClient: porkBunClient,
+		logger:     logger,
+		KubeClient: kubeClient,
 	}
 }
 
